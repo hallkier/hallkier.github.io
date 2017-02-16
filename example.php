8fe0d2a533c0af9c1ef657628961ed86131185c5
hallkier <hallkier@msu.edu>
2017-02-16 17:24:47 -0500

    Add files via upload

---
+++ example.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kieran
+ * Date: 2/15/2017
+ * Time: 11:48 AM
+ */
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>MSUFCU API/ENDPOINT</title>
+    <!-- <link href="guessing.css" type="text/css" rel="stylesheet" /> -->
+
+</head>
+<body>
+<h1>EXAMPLE ENDPOINT</h1>
+</body>
+</html>
